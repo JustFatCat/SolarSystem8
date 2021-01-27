2b5875a98cf722cb8e980c9aa2b72530aee20232
Natalis <natalis200623@gmail.com>
2021-01-28 00:41:15 +0300

    Solar System deploy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.sun = 1;
+        FactorX.moon = 3;
+        FactorX.planets = 2;
     }
 }
